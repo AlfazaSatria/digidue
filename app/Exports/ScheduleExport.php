29ca38420dfbeba074092f8725d0ac9b31b38752
Alfaza Satria <alfazasatria8@gmail.com>
2023-01-08 17:36:36 +0700

    :lollipop: feat : add heading on export excel

--- app/Exports/ScheduleExport.php
+++ app/Exports/ScheduleExport.php
@@ -4,8 +4,9 @@ namespace App\Exports;
 
 use App\Models\Schedule;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class ScheduleExport implements FromCollection
+class ScheduleExport implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -13,5 +14,35 @@ class ScheduleExport implements FromCollection
     public function collection()
     {
         return Schedule::all();
+    }
+
+    public function headings(): array
+    {
+        return [
+            'id',
+            'month_id',
+            'user_id',
+            'role_id',
+            'year',
+            'location_id',
+            'desc_job',
+            'submitted',
+            'voltage',
+            'bay_type_id',
+            'equipment_out_id',
+            'attribute',
+            'person_responsibles',
+            'start_date',
+            'end_date',
+            'start_hours',
+            'end_hours',
+            'note',
+            'notif',
+            'operation_plan',
+            'approve_id',
+            'created_at',
+            'updated_at',
+            'tag'
+        ];
     }
 }
